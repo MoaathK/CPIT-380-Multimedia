e8d0a0079c06296669d0e1e94413c582fc9fd096
Moath Alahmadi <MoaathK@users.noreply.github.com>
2024-02-19 14:56:44 +0300

    editing some files

--- diagonalRef.py
+++ diagonalRef.py
@@ -1,21 +1,13 @@
 from jes4py import *
-def mirrorVertical(src):
-
-
+def Diagonal(src):
     for sx in range(0,getWidth(src)):
         for sy in range(0,getHeight(src)):
-          if (sx > sy):
+          if (sx < sy):
             sPixel = getPixel(src,sx,sy)
             color = getColor(sPixel)
             tPixel = getPixel(src,sy,sx)
-            setColor(tPixel,color)
-          
-          
-          
-    explore(src)
+            setColor(tPixel,color)                    
     explore(src)
 
 source = makePicture("salad.jpg")
-mirrorVertical(source)
-
-
+Diagonal(source)
