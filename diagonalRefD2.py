af9eb8da8167965acc17bff10c90d6a28085e8f0
Moath Alahmadi <MoaathK@users.noreply.github.com>
2024-02-19 18:28:39 +0300

    last changes

--- diagonalRefD2.py
+++ diagonalRefD2.py
@@ -2,7 +2,7 @@ from jes4py import *
 def Diagonal(src):
     for sx in range(0,getWidth(src)):
         for sy in range(0,getHeight(src)):
-          if (sx > sy):#This condition reflect the upper tri to the lower tri
+          if (sx + sy < getHeight(src)):#This condition reflect the upper tri to the lower tri
             # if we want to change so it reflect the lowee tri to the upper tri 
             # we can make the condtion to be (sx < sy)
             sPixel = getPixel(src,sx,sy)
